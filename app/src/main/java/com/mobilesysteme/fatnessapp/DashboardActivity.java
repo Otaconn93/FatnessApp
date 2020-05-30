a5e25d2eaab7b27063ca86c7662449f536f9dd35
Otaconn93 <shamilgradov@hotmail.com>
2020-05-30 21:51:20 +0200

    updated dashboard

--- app/src/main/java/com/mobilesysteme/fatnessapp/DashboardActivity.java
+++ app/src/main/java/com/mobilesysteme/fatnessapp/DashboardActivity.java
@@ -1,7 +1,9 @@
 package com.mobilesysteme.fatnessapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,6 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -29,6 +41,41 @@ public class DashboardActivity extends AppCompatActivity {
                 openAddActivity();
             }
         });
+
+        LineChartView lineChartView = findViewById(R.id.chart);
+        String[] axisData= {"Mon","Die","Mit","Don","Fre","Sam","Son"};
+
+        int[] yAxisData = {50,20,15,30,20,60,15,40,45,10,90,18,14};
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for(int i = 0; i < axisData.length; i++){
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++){
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        lineChartView.setLineChartData(data);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        data.setAxisYLeft(yAxis);
+
+
     }
 
     private void openAddActivity() {
@@ -36,4 +83,9 @@ public class DashboardActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 }
