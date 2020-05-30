e26547ca6fcb75fe8e1b5989094a1cf56d77397a
hannes hoffmann <hannes.hoffmann@clausohm.de>
2020-05-30 15:24:09 +0200

    ADD targeted weight and deadline to SharedPreferencesUtils

--- app/src/main/java/com/mobilesysteme/fatnessapp/SharedPreferenceUtils.java
+++ app/src/main/java/com/mobilesysteme/fatnessapp/SharedPreferenceUtils.java
@@ -2,12 +2,71 @@ package com.mobilesysteme.fatnessapp;
 
 import android.content.Context;
 
+import java.util.Date;
+
 public class SharedPreferenceUtils {
 
     private static final String FILE_NAME = "FatnessPreferences";
     private static final String USER_HEIGHT_KEY = "userHeight";
     private static final String USER_WEIGHT_KEY = "userWeight";
     private static final String USER_AGE_KEY = "userAge";
+    private static final String USER_TARGETWEIGHT_KEY = "userTargetWeight";
+    private static final String USER_DEADLINE_KEY = "userDeadline";
+
+    /**
+     * extracts the users height from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     * @return the height of the user in centimeter
+     */
+    public static int getUserHeight(Context context) {
+
+        return context.getSharedPreferences(FILE_NAME, 0)
+                .getInt(USER_HEIGHT_KEY, 0);
+    }
+
+    /**
+     * extracts the users weight from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     * @return the weight of the user in kilogram
+     */
+    public static int getUserWeight(Context context) {
+
+        return context.getSharedPreferences(FILE_NAME, 0)
+                .getInt(USER_WEIGHT_KEY, 0);
+    }
+
+    /**
+     * extracts the users age from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     * @return the age of the user in years
+     */
+    public static int getUserAge(Context context) {
+
+        return context.getSharedPreferences(FILE_NAME, 0)
+                .getInt(USER_AGE_KEY, 0);
+    }
+
+    /**
+     * extracts the users targeted weight from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     * @return the targeted weight of the user in kg
+     */
+    public static int getUserTargetWeight(Context context) {
+
+        return context.getSharedPreferences(FILE_NAME, 0)
+                .getInt(USER_TARGETWEIGHT_KEY, 0);
+    }
+
+    /**
+     * extracts the users deadline for achieving his targeted weight from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     * @return the deadline of the user
+     */
+    public static Date getUserDeadline(Context context) {
+
+        return new Date(context.getSharedPreferences(FILE_NAME, 0)
+                .getLong(USER_DEADLINE_KEY, 0));
+    }
 
     /**
      * saves the users height to the SharedPreferences
@@ -49,36 +108,29 @@ public class SharedPreferenceUtils {
     }
 
     /**
-     * extracts the users height from the SharedPreferences
+     * saves the users targeted weight to the SharedPreferences
      * @param context the ApplicationContext needed to access the SharedPreferences
-     * @return the height of the user in centimeter
+     * @param targetWeight the targeted weight of the user in kg
      */
-    public static int getUserHeight(Context context) {
+    public static void saveTargetWeight(Context context, int targetWeight) {
 
-        return context.getSharedPreferences(FILE_NAME, 0)
-                .getInt(USER_HEIGHT_KEY, 0);
+        context.getSharedPreferences(FILE_NAME, 0)
+                .edit()
+                .putInt(USER_TARGETWEIGHT_KEY, targetWeight)
+                .apply();
     }
 
     /**
-     * extracts the users weight from the SharedPreferences
+     * saves the users deadline to the SharedPreferences
      * @param context the ApplicationContext needed to access the SharedPreferences
-     * @return the weight of the user in kilogram
+     * @param deadline the date of the users deadline
      */
-    public static int getUserWeight(Context context) {
+    public static void saveDeadline(Context context, Date deadline) {
 
-        return context.getSharedPreferences(FILE_NAME, 0)
-                .getInt(USER_WEIGHT_KEY, 0);
-    }
-
-    /**
-     * extracts the users age from the SharedPreferences
-     * @param context the ApplicationContext needed to access the SharedPreferences
-     * @return the age of the user in years
-     */
-    public static int getUserAge(Context context) {
-
-        return context.getSharedPreferences(FILE_NAME, 0)
-                .getInt(USER_AGE_KEY, 0);
+        context.getSharedPreferences(FILE_NAME, 0)
+                .edit()
+                .putLong(USER_DEADLINE_KEY, deadline.getTime())
+                .apply();
     }
 
     /**
@@ -114,6 +166,30 @@ public class SharedPreferenceUtils {
         context.getSharedPreferences(FILE_NAME, 0)
                 .edit()
                 .remove(USER_AGE_KEY)
+                .apply();
+    }
+
+    /**
+     * removes the targeted weight from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     */
+    public static void removeTargetWeight(Context context) {
+
+        context.getSharedPreferences(FILE_NAME, 0)
+                .edit()
+                .remove(USER_TARGETWEIGHT_KEY)
+                .apply();
+    }
+
+    /**
+     * removes the users deadline to achieve its target weight from the SharedPreferences
+     * @param context the ApplicationContext needed to access the SharedPreferences
+     */
+    public static void removeDeadline(Context context) {
+
+        context.getSharedPreferences(FILE_NAME, 0)
+                .edit()
+                .remove(USER_DEADLINE_KEY)
                 .apply();
     }
 }
