e4cc7474390725972883154d7c44c599492531f2
kevin.buecher <kevin.buecher@hochschule-stralsund.de>
2020-06-07 18:42:07 +0200

    add database context from eaten food table to calculator

--- app/src/main/java/com/mobilesysteme/fatnessapp/CalorieCalculator.java
+++ app/src/main/java/com/mobilesysteme/fatnessapp/CalorieCalculator.java
@@ -2,19 +2,22 @@ package com.mobilesysteme.fatnessapp;
 
 import android.content.Context;
 
+import com.mobilesysteme.fatnessapp.sqlObjects.EatenFood;
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CalorieCalculator {
 
     private float height = 180; // in cm
     private float weight = 78; // in kg
     private float age = 25;
-    private String gender = "m"; // w oder m
+    private Gender gender;
     private float weightGoal = 88; // in kg
     private Date goalDeadline = new Date();
     private SharedPreferenceUtils spu = new SharedPreferenceUtils();
-    private ArrayList<Integer> eatenFoodID = new ArrayList<Integer>();
+    private List<EatenFood> eatenFood = new ArrayList<EatenFood>();
     private DatabaseHelper dh;
 
     public CalorieCalculator(Context context) {
@@ -23,12 +26,11 @@ public class CalorieCalculator {
         setWeight(spu.getUserWeight(context));
         setWeightGoal(spu.getUserTargetWeight(context));
         setGoalDeadline(spu.getUserDeadline(context));
+        setGender(spu.getUserGender(context));
 
         dh = new DatabaseHelper(context);
 
-        eatenFoodID.add(1);
-        eatenFoodID.add(10);
-        eatenFoodID.add(20);
+        setEatenFood(dh.getEatenFoods());
     }
 
     /**
@@ -40,9 +42,9 @@ public class CalorieCalculator {
      * @return average calories per Day calculated from personal details
      */
     private float calculateDailyCalories(){
-        if(getGender().equals("m")){
+        if(getGender().id == 0){
             return (float) ((float) 655 + (9.6 * getWeight()) + (1.8 * getHeight()) - (4.7 * getAge()));
-        }else if(getGender().equals("w")){
+        }else if(getGender().id == 1){
             return (float) ((float) 66 + (13.7 * getWeight()) + (5 * getHeight()) - (6.8 * getAge()));
         }
         return 0;
@@ -57,8 +59,8 @@ public class CalorieCalculator {
 
         float dailyCaories = calculateDailyCalories();
         int lastCalories = 0;
-        for(int i=0; i<eatenFoodID.size()-1; i++){
-            lastCalories =+ dh.getFoodById(eatenFoodID.get(i)).getCalories();
+        for(int i=0; i<eatenFood.size()-1; i++){
+            lastCalories =+ eatenFood.get(i).getCalories();
         }
 
         return (int) (dailyCaories-lastCalories);
@@ -88,9 +90,9 @@ public class CalorieCalculator {
         this.age = age;
     }
 
-    public String getGender() { return gender; }
+    public Gender getGender() { return gender; }
 
-    public void setGender(String gender) { this.gender = gender; }
+    public void setGender(Gender gender) { this.gender = gender; }
 
     public float getWeightGoal() { return weightGoal; }
 
@@ -99,4 +101,9 @@ public class CalorieCalculator {
     public Date getGoalDeadline() { return goalDeadline; }
 
     public void setGoalDeadline(Date goalDeadline) { this.goalDeadline = goalDeadline; }
+
+    public List<EatenFood> getEatenFood() { return eatenFood; }
+
+    public void setEatenFood(List<EatenFood> eatenFood) { this.eatenFood = eatenFood; }
+
 }
