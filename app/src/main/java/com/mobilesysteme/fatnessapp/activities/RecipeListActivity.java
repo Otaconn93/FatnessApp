c537e3b173eaa0a6ca6e3a254962be9fdd375e92
Otaconn93 <shamilgradov@hotmail.com>
2020-06-19 22:21:20 +0200

    Commit intermediate results, Layout changes and adjustments

---
+++ app/src/main/java/com/mobilesysteme/fatnessapp/activities/RecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.mobilesysteme.fatnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mobilesysteme.fatnessapp.R;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
