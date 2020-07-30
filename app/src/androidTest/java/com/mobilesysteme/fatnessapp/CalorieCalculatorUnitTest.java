52134c0977bd970cebf78475ea398d45689b1d40
kevin.buecher <kevin.buecher@hochschule-stralsund.de>
2020-07-30 18:45:04 +0200

    ADD extra tests to test calories per day and calories left in each case

--- app/src/androidTest/java/com/mobilesysteme/fatnessapp/CalorieCalculatorUnitTest.java
+++ app/src/androidTest/java/com/mobilesysteme/fatnessapp/CalorieCalculatorUnitTest.java
@@ -32,6 +32,9 @@ public class CalorieCalculatorUnitTest {
         databaseHelper.refillDatabase();
     }
 
+    /**
+     * Test with normal values and male gender to calculate needed calories
+     */
     @Test
     public void checkCaloriesPerDay_Male() {
 
@@ -39,6 +42,9 @@ public class CalorieCalculatorUnitTest {
         assertEquals(28890, calorieCalculator.getDailyCalories());
     }
 
+    /**
+     * Test with normal values and male gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeft_Male(){
 
@@ -47,6 +53,9 @@ public class CalorieCalculatorUnitTest {
         assertEquals(27890, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Test with normal values and female gender to calculate needed calories
+     */
     @Test
     public void checkCaloriesPerDay_Female() {
 
@@ -54,6 +63,9 @@ public class CalorieCalculatorUnitTest {
         assertEquals(29013, calorieCalculator.getDailyCalories());
     }
 
+    /**
+     * Test with normal values and female gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeft_Female(){
 
@@ -62,34 +74,100 @@ public class CalorieCalculatorUnitTest {
         assertEquals(28013, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Test with minimum values and male gender to calculate needed calories
+     */
+    @Test
+    public void checkDailyCaloriesWithMinValues_Male() {
+
+        setSharedPrefencesValues(50, 25, Gender.MALE, new Date(new Date().getTime() + DateUtils.WEEK_IN_MILLI_SECS), 175, 75);
+        assertEquals(28890, calorieCalculator.getDailyCalories());
+    }
+
+    /**
+     * Test with minimum values and male gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeftWithMinValues_Male(){
 
         setSharedPrefencesValues(2, 0, Gender.MALE, new Date(new Date().getTime() + DateUtils.DAY_IN_MILLI_SECS), 50, 5);
-        assertEquals(23912, calorieCalculator.getDailyCaloriesLeft());
+        databaseHelper.addEatenFood(1, 1000, new Date());
+        assertEquals(27890, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Test with minimum values and male gender to calculate needed calories
+     */
+    @Test
+    public void checkDailyCaloriesWithMinValues_Female(){
+
+        setSharedPrefencesValues(2, 0, Gender.FEMALE, new Date(new Date().getTime() + DateUtils.DAY_IN_MILLI_SECS), 50, 5);
+        assertEquals(23491, calorieCalculator.getDailyCalories());
+    }
+
+    /**
+     * Test with minimum values and female gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeftWithMinValues_Female(){
 
         setSharedPrefencesValues(2, 0, Gender.FEMALE, new Date(new Date().getTime() + DateUtils.DAY_IN_MILLI_SECS), 50, 5);
-        assertEquals(23491, calorieCalculator.getDailyCaloriesLeft());
+        databaseHelper.addEatenFood(1, 1000, new Date());
+        assertEquals(22491, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Test with maximum values and male gender to calculate needed calories
+     */
+    @Test
+    public void checkDailyCaloriesWithMaxValues_Male(){
+
+        setSharedPrefencesValues(499, 150, Gender.MALE, new Date(new Date().getTime() + DateUtils.YEAR_IN_MILLI_SECS * 9L), 300, 500);
+        assertEquals(5282, calorieCalculator.getDailyCalories());
+    }
+
+    /**
+     * Test with maximum values and male gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeftWithMaxValues_Male(){
 
         setSharedPrefencesValues(499, 150, Gender.MALE, new Date(new Date().getTime() + DateUtils.YEAR_IN_MILLI_SECS * 9L), 300, 500);
-        assertEquals(5282, calorieCalculator.getDailyCaloriesLeft());
+        databaseHelper.addEatenFood(1, 1000, new Date());
+        assertEquals(4282, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Test with minimum values and female gender to calculate needed calories
+     */
+    @Test
+    public void checkDailyCaloriesWithMaxValues_Female(){
+
+        setSharedPrefencesValues(499, 150, Gender.FEMALE, new Date(new Date().getTime() + DateUtils.YEAR_IN_MILLI_SECS * 9L), 300, 500);
+        assertEquals(7384, calorieCalculator.getDailyCalories());
+    }
+
+    /**
+     * Test with maximum values and female gender to calculate calories left
+     */
     @Test
     public void checkDailyCaloriesLeftWithMaxValues_Female(){
 
         setSharedPrefencesValues(499, 150, Gender.FEMALE, new Date(new Date().getTime() + DateUtils.YEAR_IN_MILLI_SECS * 9L), 300, 500);
-        assertEquals(7384, calorieCalculator.getDailyCaloriesLeft());
+        databaseHelper.addEatenFood(1, 1000, new Date());
+        assertEquals(6384, calorieCalculator.getDailyCaloriesLeft());
     }
 
+    /**
+     * Method to put test data into SharedPreferences
+     *
+     * @param weight
+     * @param age
+     * @param gender
+     * @param deadline
+     * @param height
+     * @param targetWeight
+     */
     private void setSharedPrefencesValues(int weight, int age, Gender gender, Date deadline, int height, int targetWeight){
 
         SharedPreferenceUtils.saveUserWeightNow(context, weight);
